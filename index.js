da6fa404a1c3d7b363b7a422c705fa602a1bf602
hidehic0 <hidehico.ooba@gmail.com>
2026-07-05 18:23:26 +0900

    feat: 初期機能が完成

---
+++ index.js
@@ -0,0 +1,92 @@
+const SKIP_CONTESTS = new Set([316]);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ignore_contests = document.getElementById("ignore_contests"),
+    response_div = document.getElementById("response"),
+    contest_lower_dom = document.getElementById("contest_lower"),
+    contest_upper_dom = document.getElementById("contest_upper"),
+    generate_button = document.getElementById("generate_button");
+
+  function get_ignore_contests() {
+    return new Set(
+      (ignore_contests.value.match(/ABC\d\d\d/g) ?? []).map((x) => Number(x.slice(3))),
+    );
+  }
+
+  function add_ignore_contest(contest_id) {
+    ignore_contests.value += `${ignore_contests.value ? "\n" : ""}ABC${contest_id}`;
+    localStorage.setItem("ignore_contests", ignore_contests.value);
+  }
+
+  function rand_range(l, u) {
+    return Math.floor(Math.random() * (u - l) + l);
+  }
+
+  function error_html(message) {
+    return `<p class="error_message">${message}</p>`;
+  }
+
+  const fields = { contest_lower: 212, contest_upper: 450, ignore_contests: "" };
+
+  for (let field in fields) {
+    const el = document.getElementById(field);
+
+    if (!el) continue;
+
+    const saved = localStorage.getItem(field);
+
+    if (saved !== null) {
+      el.value = saved;
+    } else {
+      el.value = fields[field];
+    }
+
+    el.addEventListener("input", () => {
+      localStorage.setItem(field, el.value);
+    });
+  }
+
+  if (generate_button) {
+    generate_button.addEventListener("click", () => {
+      const contest_lower_value = contest_lower_dom.value,
+        contest_upper_value = contest_upper_dom.value;
+      const ignore_contests = get_ignore_contests();
+
+      if (!contest_lower_value || !contest_upper_value) {
+        response_div.innerHTML = error_html("使うコンテストの下限と上限を設定してください。");
+        return;
+      }
+
+      const contest_lower = Number(contest_lower_value),
+        contest_upper = Number(contest_upper_value);
+
+      if (contest_lower > contest_upper) {
+        response_div.innerHTML = error_html("使うコンテストの下限を上限より小さくしてください。");
+        return;
+      }
+
+      if (contest_lower <= 0) {
+        response_div.innerHTML = error_html("使うコンテストの下限は1以上でないといけません。");
+        return;
+      }
+
+      let available_contests = [];
+
+      for (let i = contest_lower; i <= contest_upper; i++) {
+        if (!SKIP_CONTESTS.has(i) && !ignore_contests.has(i)) available_contests.push(i);
+      }
+
+      if (available_contests.length == 0) {
+        response_div.innerHTML = error_html("抽出可能なコンテストがありません。");
+        return;
+      }
+
+      const contest_id = available_contests[rand_range(0, available_contests.length)];
+
+      response_div.innerHTML = `<a href="https://atcoder.jp/contests/abc${contest_id}" target="_blank">ABC${contest_id}</a><button id="add_ignore_list_button">add to ignore list</button>`;
+      response_div
+        .querySelector("#add_ignore_list_button")
+        .addEventListener("click", () => add_ignore_contest(contest_id));
+    });
+  }
+});
